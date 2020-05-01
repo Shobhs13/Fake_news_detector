51a231159c17f62031ff68853fdeacb6b30d1336
Shobhit Sinha <46240995+Shobhs13@users.noreply.github.com>
2020-05-02 02:30:23 +0530

    Final Commit
    Created the following:
    * split the data into test and train models
    * Initialized: TfidfVectoreizer - transfer text to vectors 
    * Initialized Passive Aggressive Classifier which is used to predict the following.
    * Accuracy: 93.05%

--- Fake_new_dector.py
+++ Fake_new_dector.py
@@ -21,3 +21,26 @@ df = pd.read_csv('C:\\Users\\HP\\Downloads\\news.csv')
 print(df.shape)
 print(df.head())
 
+
+#Defining Labels
+labels=df.label
+labels.head()
+
+#Splitting of Data into test and train case.
+x_train,x_test,y_train,y_test=train_test_split(df['text'],labels,test_size=0.2, random_state=7)
+
+#Initialzing TfidfVectorizer
+tfidf_vectorizer= TfidfVectorizer(stop_words='english', max_df=0.7)
+
+#tranforing data set into test and train sets
+tfidf_train=tfidf_vectorizer.fit_transform(x_train)
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+#Initializing Passive Aggresive Classifier
+pac=PassiveAggressiveClassifier(max_iter=1000)
+pac.fit(tfidf_train,y_train)
+
+#predicting Data Set
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
